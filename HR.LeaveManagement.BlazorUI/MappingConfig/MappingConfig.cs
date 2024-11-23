a8b9c6d3f59b9f85672af529f2102ba1681b00a6
gaurav lokhande <lokhandeg570@gmail.com>
2024-11-23 12:55:45 +0530

    partial UI us completed

---
+++ HR.LeaveManagement.BlazorUI/MappingConfig/MappingConfig.cs
@@ -0,0 +1,15 @@
+﻿using HR.LeaveManagement.BlazorUI.Models.LeaveTypes;
+using AutoMapper;
+using HR.LeaveManagement.BlazorUI.Services.Base;
+using MyNamespace;
+
+namespace HR.LeaveManagement.BlazorUI.MappingConfig
+{
+    public class MappingConfig :Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<LeaveTypeDto, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
